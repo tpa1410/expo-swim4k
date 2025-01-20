13453fe3d94cdfa4892d33a00bce259339b73821
tpa1410 <120876178+tpa1410@users.noreply.github.com>
2025-01-20 20:06:24 +0700

    Update NewsCard.tsx

--- src/components/news/NewsCard.tsx
+++ src/components/news/NewsCard.tsx
@@ -3,13 +3,21 @@ import React from 'react'
 import { images } from '@/constants'
 import { useRouter } from 'expo-router'
 import { ERouteTable } from '@/constants/route-table'
+import { INew } from '@/constants/common'
 
-type Props = {}
+type Props = {
+  data: INew
+}
 
-const NewsCard = (props: Props) => {
+const NewsCard = ({ data }: Props) => {
   const router = useRouter()
   const onNewDetail = () => {
-    router.push(ERouteTable.NEWS_DETAIL)
+    router.push({
+      pathname: ERouteTable.NEWS_DETAIL,
+      params: {
+        id: data.id,
+      },
+    })
   }
 
   return (
@@ -17,16 +25,16 @@ const NewsCard = (props: Props) => {
       <View className="w-full gap-3">
         <View className="rounded-2xl overflow-hidden w-full min-h-52">
           <ImageBackground
-            source={images.practice1}
+            source={{ uri: data.thumbnail }}
             className="flex-1 flex-row items-start justify-end p-3 object-cover"
           ></ImageBackground>
         </View>
         <View className="">
           <Text className="text-white font-semibold text-base" numberOfLines={2}>
-            Cách bơi ngửa cho người mới bắt đầu
+            {data.title}
           </Text>
           <Text className="text-text-default text-sm" numberOfLines={2}>
-            Học tất cả các bài tập bơi cần thiết để thành thạo nghệ thuật bơi lội tại đây!
+            {data.description}
           </Text>
         </View>
       </View>
