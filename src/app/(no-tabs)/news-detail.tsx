11e6d006f4eb909d28fd9d140bff8f74f4bda612
tpa1410 <120876178+tpa1410@users.noreply.github.com>
2025-01-20 20:07:06 +0700

    Update news-detail.tsx

--- src/app/(no-tabs)/news-detail.tsx
+++ src/app/(no-tabs)/news-detail.tsx
@@ -1,42 +1,62 @@
-import {
-  Image,
-  ImageBackground,
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  Text,
-  View,
-} from 'react-native'
-import React from 'react'
-import { images } from '@/constants'
+import { Alert, SafeAreaView, ScrollView, StatusBar, Text, View } from 'react-native'
+import React, { useCallback, useState } from 'react'
+import YoutubePlayer, { PLAYER_STATES } from 'react-native-youtube-iframe'
 
-import BackButton from '@/components/BackButton'
+import { useLocalSearchParams } from 'expo-router'
+import { news } from '@/constants/common'
+import Header from '@/components/Header'
+import { wp } from '@/helpers/common'
 
 type Props = {}
 
 const NewDetail = (props: Props) => {
+  const { id } = useLocalSearchParams<{
+    id: string
+  }>()
+
+  const newData = news.find((item) => item.id === Number(id))
+  const [playing, setPlaying] = useState(false)
+
+  const onStateChange = useCallback((state: PLAYER_STATES) => {
+    if (state === 'ended') {
+      setPlaying(false)
+      Alert.alert('video has finished playing!')
+    }
+  }, [])
   return (
     <ScrollView className="flex-1 bg-background" showsVerticalScrollIndicator={false}>
-      <ImageBackground source={images.practice1} className="w-full h-64">
-        <View className="absolute inset-0 bg-black opacity-45" />
-        <SafeAreaView className={'pb-8 z-50'} style={{ paddingTop: StatusBar.currentHeight }}>
-          <BackButton />
-        </SafeAreaView>
-      </ImageBackground>
-      <View className="p-4 gap-4">
-        <Text className="text-xl text-primary-500 font-semibold">Bơi lội và lợi ích của nó</Text>
-        <Text className="text-base text-white">
-          Bơi lội không chỉ giúp cải thiện sức khỏe mà còn giúp con người có thêm giá trị đạo đức
-          học. Lorem ipsum dolor sit amet consectetur. Senectus ac magna egestas sit lobortis.
-          Ultrices augue sit dictumst volutpat nullam. Semper ac tristique gravida leo. Dui mauris
-          mattis viverra bibendum eros amet. Turpis ultricies a cum pellentesque suspendisse porta
-          ipsum est. Elit facilisis maecenas lorem enim dictumst. Lorem ipsum dolor sit amet
-          consectetur. Senectus ac magna egestas sit lobortis. Ultrices augue sit dictumst volutpat
-          nullam. Semper ac tristique gravida leo. Dui mauris mattis viverra bibendum eros amet.
-          Turpis ultricies a cum pellentesque suspendisse porta ipsum est. Elit facilisis maecenas
-          lorem enim dictumst.
-        </Text>
-      </View>
+      <SafeAreaView className={'pb-8'} style={{ paddingTop: StatusBar.currentHeight }}>
+        <View className="px-4">
+          <Header title={newData?.title || ''} hasBack />
+        </View>
+        <View className="mt-4">
+          <YoutubePlayer
+            height={240}
+            width={wp(100)}
+            play={playing}
+            videoId={newData?.youtubeId || '8gVvGAWJGps'}
+            onChangeState={onStateChange}
+          />
+        </View>
+        <View className="p-4 gap-4">
+          <Text className="text-xl text-primary-500 font-semibold">{newData?.title || ''}</Text>
+          <Text className="text-base text-white">{newData?.description || ''}</Text>
+          <View className="gap-4">
+            {newData?.body.map((item, idx) => (
+              <View className="" key={`${item.title}-${idx}`}>
+                <Text className="text-base text-primary-500 font-semibold">{item.title || ''}</Text>
+                <View className="ml-4 gap-1">
+                  {item.content.map((cnt) => (
+                    <Text className="text-base text-white" key={cnt}>
+                      + {cnt}
+                    </Text>
+                  ))}
+                </View>
+              </View>
+            ))}
+          </View>
+        </View>
+      </SafeAreaView>
     </ScrollView>
   )
 }
