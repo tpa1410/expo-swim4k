de1a97ecc7e3da13222960774a2f19c7155d90a0
tpa1410 <120876178+tpa1410@users.noreply.github.com>
2025-01-20 20:06:11 +0700

    Update LatestCard.tsx

--- src/components/news/LatestCard.tsx
+++ src/components/news/LatestCard.tsx
@@ -3,26 +3,33 @@ import React from 'react'
 import { images } from '@/constants'
 import { useRouter } from 'expo-router'
 import { ERouteTable } from '@/constants/route-table'
+import { INew } from '@/constants/common'
 
-type Props = {}
+type Props = {
+  data: INew
+}
 
-const LatestCard = (props: Props) => {
+const LatestCard = ({ data }: Props) => {
   const router = useRouter()
   const onNewDetail = () => {
-    router.push(ERouteTable.NEWS_DETAIL)
+    router.push({
+      pathname: ERouteTable.NEWS_DETAIL,
+      params: {
+        id: data.id,
+      },
+    })
   }
 
   return (
     <TouchableOpacity onPress={onNewDetail}>
       <View className="w-full gap-2">
-        <Image source={images.news1} className="w-40 h-24 rounded-xl" />
+        <Image source={{ uri: data.thumbnail }} className="w-40 h-24 rounded-xl" />
         <View>
-          <Text className="text-sm text-white font-semibold">
-            Giá trị đạo đức học được từ bơi lội
+          <Text className="text-sm text-white font-semibold" numberOfLines={1}>
+            {data.title}
           </Text>
           <Text className="text-xs text-text-default" numberOfLines={2}>
-            Bơi lội không chỉ giúp cải thiện sức khỏe mà còn giúp con người có thêm giá trị đạo đức
-            học.
+            {data.description}
           </Text>
         </View>
       </View>
