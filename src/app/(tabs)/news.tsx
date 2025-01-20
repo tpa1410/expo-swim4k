270797396c84434dff25beb94b2eb3278e05d75d
tpa1410 <120876178+tpa1410@users.noreply.github.com>
2025-01-20 20:07:19 +0700

    Update news.tsx

--- src/app/(tabs)/news.tsx
+++ src/app/(tabs)/news.tsx
@@ -3,8 +3,11 @@ import React from 'react'
 import Header from '@/components/Header'
 import LatestCard from '@/components/news/LatestCard'
 import NewsCard from '@/components/news/NewsCard'
+import { getRandomElements } from '@/helpers/function'
+import { INew, news } from '@/constants/common'
 
-const Bookmark = () => {
+const News = () => {
+  const latestNews = getRandomElements<INew>(news, 4)
   return (
     <ScrollView className="flex-1 bg-background" showsVerticalScrollIndicator={false}>
       <SafeAreaView className={'pb-8'} style={{ paddingTop: StatusBar.currentHeight }}>
@@ -12,9 +15,9 @@ const Bookmark = () => {
         <View className="gap-4 pl-4">
           <Text className="text-2xl text-primary-500 font-bold">MỚI THÊM</Text>
           <ScrollView horizontal showsHorizontalScrollIndicator={false}>
-            {[1, 2, 3, 4, 5].map((item) => (
-              <View key={item} className="w-40 mr-4">
-                <LatestCard />
+            {latestNews.map((item) => (
+              <View key={`latest-${item.id}`} className="w-40 mr-4">
+                <LatestCard data={item} />
               </View>
             ))}
           </ScrollView>
@@ -22,9 +25,9 @@ const Bookmark = () => {
         <View className="gap-4 px-4 mt-7">
           <Text className="text-2xl text-primary-500 font-bold">TẤT CẢ BÀI VIẾT</Text>
           <View className="gap-4">
-            {[1, 2, 3, 4, 5].map((item) => (
-              <View key={item} className="">
-                <NewsCard />
+            {news.map((item) => (
+              <View key={`all-${item.id}`} className="">
+                <NewsCard data={item} />
               </View>
             ))}
           </View>
@@ -34,4 +37,4 @@ const Bookmark = () => {
   )
 }
 
-export default Bookmark
+export default News
